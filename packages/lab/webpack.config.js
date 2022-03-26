eca60c9f4dbe368f3c739e4605ddec5c69313752
Yusef Sohail <yusefsohail@gmail.com>
2022-03-25 21:50:41 -0700

    add lab project

---
+++ packages/lab/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const html = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  resolve: {
+    extensions: ["ts", "js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new html()],
+};
