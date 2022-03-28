d571f7fafc88b977cf1a2067bb4eafa9dc6b0ba3
Yusef Sohail <yusefsohail@gmail.com>
2022-03-28 08:28:37 -0700

    add more DFA tests

--- packages/compiler/tests/CompilerTests.cpp
+++ packages/compiler/tests/CompilerTests.cpp
@@ -6,6 +6,11 @@ TEST(CompilerTests, NFATests) {
 	nfa.Append(NFA::FromRegularExpression("def"));
 
 	DFA dfa = DFA::FromNFA(nfa);
-
 	EXPECT_EQ(dfa.Match("abcdef"), true);
+
+	nfa.Union(NFA::FromRegularExpression("ghjklm"));
+	dfa = DFA::FromNFA(nfa);
+	EXPECT_EQ(dfa.Match("abcdef"), true);
+	EXPECT_EQ(dfa.Match("ghjklm"), true);
+	EXPECT_EQ(dfa.Match("abcghj"), false);
 }
