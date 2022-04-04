7c130a7d0c9776f9968236b832d03cba9442e4b8
Yusef Sohail <YusefSohail@gmail.com>
2022-04-04 15:44:35 -0700

    formatting

--- packages/compiler/src/RegularExpression/Tokenizer.h
+++ packages/compiler/src/RegularExpression/Tokenizer.h
@@ -4,16 +4,16 @@
 
 namespace RegularExpression {
 
-    enum class TokenType {
-        Character,
-        EndOfFile,
-        LeftParenthesis,
-        Quantifier,
-        RightParenthesis,
-    };
+enum class TokenType {
+    Character,
+    EndOfFile,
+    LeftParenthesis,
+    Quantifier,
+    RightParenthesis,
+};
 
-    using Token = std::pair<TokenType, std::string>;
+using Token = std::pair<TokenType, std::string>;
 
-    Token Tokenize(std::stringstream& source);
+Token Tokenize(std::stringstream& source);
 
 };
