230a438c008322ed0bde7fac844b6275fe55f8c1
Yusef Sohail <YusefSohail@gmail.com>
2022-04-06 10:33:42 -0700

    use ast to create regular expression

--- packages/compiler/src/RegularExpression/RegularExpression.cpp
+++ packages/compiler/src/RegularExpression/RegularExpression.cpp
@@ -1,20 +1,43 @@
 #include "RegularExpression.h"
+#include "Parser.h"
 
 namespace RegularExpression {
 
-DFA Create(std::string expression) {
+NFA Create(char character) {
 	NFA nfa;
-
 	nfa.initialState = nfa.AddState();
+	nfa.acceptingState = nfa.AddState();
+	nfa.AddTransition(nfa.initialState, nfa.acceptingState, character);
 
-	uint32_t currentState = nfa.initialState;
-	for (char character : expression) {
-		uint32_t nextState = nfa.AddState();
-		nfa.AddTransition(currentState, nextState, character);
-		currentState = nextState;
+	return nfa;
+}
+
+NFA Create(AST::Expression& ast) {
+	return Create(ast.character[0]);
+}
+
+NFA Create(AST::RegularExpression& ast) {
+	NFA nfa;
+	nfa.initialState = nfa.AddState();
+	nfa.acceptingState = nfa.initialState;
+
+	AST::Sequence* current = ast.sequence;
+	while (current != nullptr) {
+		if (current->lhs != nullptr) {
+			nfa.Append(Create(*current->lhs));
+		}
+		
+		current = current->rhs;
 	}
 
-	nfa.acceptingState = currentState;
+	return nfa;
+}
+
+DFA Create(std::string expression) {
+	TokenStream<Token> stream(expression, Tokenize);
+	ParserContext context{ stream.Next(), stream };
+	AST::RegularExpression* ast = ParseRegularExpression(context);
+	NFA nfa = Create(*ast);
 
 	return DFA::FromNFA(nfa);
 }
--- packages/compiler/src/RegularExpression/tests/RegularExpressionTests.cpp
+++ packages/compiler/src/RegularExpression/tests/RegularExpressionTests.cpp
@@ -1,16 +1,9 @@
 #include <gtest/gtest.h>
-#include "RegularExpression/Parser.h"
 #include  "RegularExpression/RegularExpression.h"
 
 using namespace RegularExpression;
 
 TEST(Compiler, RegularExpressionTokenizer) {
-	TokenStream<Token> stream("aa+bb", Tokenize);
-	ParserContext context{ stream.Next(), stream };
-
-	AST::RegularExpression* ast = ParseRegularExpression(context);
-	ASSERT_EQ(ast->sequence->lhs->character, "a");
-
 	DFA dfa = RegularExpression::Create("abc");
 
 	EXPECT_EQ(dfa.Match("abc"), true);
