3bd28a4b4efc02d829c87c9896d23c0b0547a0b1
Yusef Sohail <YusefSohail@gmail.com>
2022-04-05 22:23:30 -0700

    update parser

--- packages/compiler/src/RegularExpression/Parser.cpp
+++ packages/compiler/src/RegularExpression/Parser.cpp
@@ -1,70 +1,65 @@
-#pragma once
-#include "Parser.h"
 #include <assert.h>
-#include <vector>
-#include <map>
 #include <utility>
-#include <sstream>
-#include "TokenStream.h"
+#include "Parser.h"
 
 namespace RegularExpression {
 
-    void* ParseOptionalQuantifier(ParserContext& context) {
-        switch (context.token.first) {
-        case TokenType::Quantifier:
-        {
-            assert(context.token.first == TokenType::Quantifier);
-            auto P0 = context.Use();
+void* ParseOptionalQuantifier(ParserContext& context) {
+        switch(context.token.first) {
+                case TokenType::Quantifier:
+                {
+                        assert(context.token.first == TokenType::Quantifier);
+                        auto P0 = context.Use();
 
-            { return nullptr; }
+                        { return nullptr; }
+                }
+                default:
+                        return nullptr;
         }
-        default:
-            return nullptr;
-        }
-    }
+}
 
-    void* ParseExpression(ParserContext& context) {
-        switch (context.token.first) {
-        case TokenType::Character:
-        {
-            assert(context.token.first == TokenType::Character);
-            auto P0 = context.Use();
-            auto P1 = ParseOptionalQuantifier(context);
+void* ParseExpression(ParserContext& context) {
+        switch(context.token.first) {
+                case TokenType::Character:
+                {
+                        assert(context.token.first == TokenType::Character);
+                        auto P0 = context.Use();
+                        auto P1 = ParseOptionalQuantifier(context);
 
-            { return nullptr; }
+                        { return nullptr; }
+                }
+                default:
+                        return nullptr;
         }
-        default:
-            return nullptr;
-        }
-    }
+}
 
-    void* ParseOptionalSequence(ParserContext& context) {
-        switch (context.token.first) {
-        case TokenType::Character:
-        {
-            auto P0 = ParseExpression(context);
-            auto P1 = ParseOptionalSequence(context);
+void* ParseOptionalSequence(ParserContext& context) {
+        switch(context.token.first) {
+                case TokenType::Character:
+                {
+                        auto P0 = ParseExpression(context);
+                        auto P1 = ParseOptionalSequence(context);
 
-            { return nullptr; }
+                        { return nullptr; }
+                }
+                default:
+                        return nullptr;
         }
-        default:
-            return nullptr;
-        }
-    }
+}
 
-    void* ParseRegularExpression(ParserContext& context) {
-        switch (context.token.first) {
-        case TokenType::Character:
-        {
-            auto P0 = ParseOptionalSequence(context);
-            assert(context.token.first == TokenType::EndOfFile);
-            auto P1 = context.Use();
+void* ParseRegularExpression(ParserContext& context) {
+        switch(context.token.first) {
+                case TokenType::Character:
+                {
+                        auto P0 = ParseOptionalSequence(context);
+                        assert(context.token.first == TokenType::EndOfFile);
+                        auto P1 = context.Use();
 
-            { return nullptr; }
+                        { return nullptr; }
+                }
+                default:
+                        return nullptr;
         }
-        default:
-            return nullptr;
-        }
-    }
+}
 
 }
--- packages/compiler/src/RegularExpression/Parser.h
+++ packages/compiler/src/RegularExpression/Parser.h
@@ -3,30 +3,26 @@
 
 namespace RegularExpression {
 
-    enum class TokenType {
+enum class TokenType {
         Character,
         EndOfFile,
         Quantifier,
-    };
+};
 
-    using Token = std::pair<TokenType, std::string>;
+using Token = std::pair<TokenType, std::string>;
 
-    Token Tokenize(std::stringstream& source);
+Token Tokenize(std::stringstream& source);
 
-    class ParserContext {
-    public:
+class ParserContext {
+public:
         Token token;
         TokenStream<Token>& stream;
-        Token Use() { 
-            Token old = this->token;
-            this->token = this->stream.Next();
-            return old;
-        }
-    };
+        Token Use() { auto old = this->token; this->token = this->stream.Next(); return old; }
+};
 
-    void* ParseOptionalQuantifier(ParserContext& context);
-    void* ParseExpression(ParserContext& context);
-    void* ParseOptionalSequence(ParserContext& context);
-    void* ParseRegularExpression(ParserContext& context);
+void* ParseOptionalQuantifier(ParserContext& context);
+void* ParseExpression(ParserContext& context);
+void* ParseOptionalSequence(ParserContext& context);
+void* ParseRegularExpression(ParserContext& context);
 
 }
