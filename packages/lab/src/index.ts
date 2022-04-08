da365c709bd10094a22366bbd0c97ef9363f8bcb
Yusef Sohail <YusefSohail@gmail.com>
2022-04-08 14:20:28 -0700

    initialize a red window w/ opegl context

--- packages/lab/src/index.ts
+++ packages/lab/src/index.ts
@@ -1,4 +1,14 @@
+interface Window {
+    Module: { canvas: HTMLCanvasElement };
+}
+
 function main() {
+    window.Module = {
+        canvas : document.createElement("canvas")
+    };
+
+    document.body.appendChild(window.Module.canvas);
+
     const script: HTMLScriptElement = document.createElement("script");
     script.src = "/packages/lawless/lawless.js"
     document.body.appendChild(script);
--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <assert.h>
+#ifdef EMSCRIPTEN
+#include <GLES3/gl3.h>
+#else
+#include <glad/glad.h>
+#endif
+#include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Welcome to Lawless" << std::endl;
+    assert(glfwInit());
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Lawless", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+#ifndef EMSCRIPTEN
+    assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
+#endif
+
+    glClearColor(1, 0, 0, 1);
+   
+#ifndef EMSCRIPTEN
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+#else
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+#endif
 
     return 0;
 }
