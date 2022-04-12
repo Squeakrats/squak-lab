e37b61c4661e131e733467a9f84bd5a54905cd2d
Yusef Sohail <YusefSohail@gmail.com>
2022-04-11 18:58:20 -0700

    delete globals

--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -1,33 +1,28 @@
-#include <memory>
 #include <iostream>
-#include <assert.h>
 #include "gl/Renderer.h"
 #include "Window.h"
 #include "SceneNode.h"
 
-std::unique_ptr<Window> window = nullptr;
-std::unique_ptr<gl::Renderer> renderer{};
-std::unique_ptr<SceneNode> scene{};
-std::unique_ptr<Matrix4> camera{};
-uint32_t frameId = 0;
-
-void tick() {
-    scene->transform.SetPosition(std::sin(static_cast<float>(frameId) / 9000) * 300.0f, 0.0f, 0.f);
-    renderer->Render(*camera, *scene);
+void tick(SceneNode& scene, gl::Renderer& renderer, Matrix4& camera, uint32_t& frameId) {
+    scene.transform.SetPosition(std::sin(static_cast<float>(frameId) / 9000) * 300.0f, 0.0f, 0.f);
+    renderer.Render(camera, scene);
     frameId++;
 }
 
 int main(int argc, char* argv[]) {
-    window = std::make_unique<Window>(Window::Create(600, 600, "Lawless"));
+    Window window = Window::Create(600, 600, "Lawless");
     std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
 
-    renderer = std::make_unique<gl::Renderer>();
-    camera = std::make_unique<Matrix4>(Matrix4::Orthographic(-300, 300, -300, 300, -300, 300));
+    gl::Renderer renderer{};
+    Matrix4 camera = Matrix4::Orthographic(-300, 300, -300, 300, -300, 300);
 
-    scene = std::make_unique<SceneNode>(Matrix4::Identity());
-    scene->geometry = std::make_unique<Geometry>(Geometry::CreatePlane(200, 200));
+    SceneNode scene{ Matrix4::Identity() };
+    scene.geometry = std::make_shared<Geometry>(Geometry::CreatePlane(200, 200));
 
-    window->Tick(tick);
+    uint32_t frameId = 0;
+    window.Tick([&scene, &renderer, &camera, &frameId]() {
+        tick(scene, renderer, camera, frameId);
+    });
 
     return 0;
 }
