85d25457f1249d286286491a7d98a4a0db61b80f
Yusef Sohail <YusefSohail@gmail.com>
2022-04-13 18:27:43 -0700

    update shaders for webgl/native compat

--- packages/gl/src/shaders.cpp
+++ packages/gl/src/shaders.cpp
@@ -2,15 +2,15 @@
 
 namespace gl::shaders {
     
-const char* const vertex = R"ESC(
-#version 330 core
+const char* const vertex = R"ESC(#version 300 es
+precision highp float;
 layout(location = 0) in vec3 aPosition;
 layout(location = 1) in vec3 aNormal;
 
 uniform mat4 uPerspective;
 uniform mat4 uModel;
 
-varying vec3 vNormal;
+out vec3 vNormal;
 
 void main() {
     vNormal = aNormal;
@@ -18,14 +18,16 @@ void main() {
 }
 )ESC";
 
-const char* const fragment = R"ESC(
-#version 330 core
+const char* const fragment = R"ESC(#version 300 es
+precision highp float;
 
-varying vec3 vNormal;
+in vec3 vNormal;
+
+out vec4 position;
 
 void main() {
-    float s = max(0, dot(normalize(vNormal), vec3(0.0, 1.0, 0.0)));
-    gl_FragColor = vec4(s * vec3(0.3, 0.2, 0.2) + vec3(0.2, 0.4, 0.6), 1.0);
+    float s = dot(normalize(vNormal), vec3(0.0, 1.0, 0.0));
+    position = vec4(s * vec3(0.3, 0.2, 0.2) + vec3(0.2, 0.4, 0.6), 1.0);
 }
 )ESC";
 
