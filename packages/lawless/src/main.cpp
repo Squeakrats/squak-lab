ffb8b83dbff7d05f710acc0cb9eb1c931957d772
Yusef Sohail <YusefSohail@gmail.com>
2022-04-14 12:00:45 -0700

    tidy main

--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -7,18 +7,16 @@
 
 std::unique_ptr<Engine> engine{};
 
-void main_loop() {
-    engine->Tick();
-}
+void main_loop() { engine->Tick(); }
+
+#ifdef EMSCRIPTEN
+std::string assetDir = "./";
+#else
+std::string assetDir = "..\\..\\..\\..\\assets\\";
+#endif
 
 int main(int argc, char* argv[]) {
     std::cout << "Working Directory : " << std::filesystem::current_path() << std::endl;
-
-#ifndef EMSCRIPTEN
-    std::string path = "..\\..\\..\\..\\assets\\suzanne.glb";
-#else
-    std::string path = "./suzanne.glb";
-#endif
 
     engine = std::make_unique<Engine>(Engine::Create(600, 600, "Lawless"));
     std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
@@ -26,7 +24,7 @@ int main(int argc, char* argv[]) {
     engine->SetRenderer(std::make_shared<gl::Renderer>());
     engine->SetCamera(std::make_shared<Matrix4>(Matrix4::Perspective(110.0f, 1, 100)));
 
-    std::shared_ptr<SceneNode> suzanne = gltf::Load(path);
+    std::shared_ptr<SceneNode> suzanne = gltf::Load(assetDir + "suzanne.glb");
 
     std::shared_ptr<SceneNode> node1 = std::make_shared<SceneNode>();
     node1->transform.position = Vector3(4, 0.0, 0.0);
@@ -37,19 +35,17 @@ int main(int argc, char* argv[]) {
     node2->AddChild(suzanne);
 
     auto scene = engine->GetScene();
+    scene->transform.position = Vector3(0.0f, -0.0f, -30.0);
+
     scene->AddChild(node1);
     scene->AddChild(node2);
-
-    scene->transform.position = Vector3(0.0f, -0.0f, -30.0);
 
     engine->OnTick([&scene](float deltaMs) {
         scene->transform.rotation.y += 0.001f * deltaMs;
     });
 
 #ifndef EMSCRIPTEN
-    while (engine->isRunning()) {
-        main_loop();
-    }
+    while (engine->isRunning()) { main_loop(); }
 #else
     emscripten_set_main_loop(main_loop, 0, true);
 #endif
