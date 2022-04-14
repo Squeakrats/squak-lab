e21de3839ecfb6978d397b7bbc0d5ef0a0ba9c89
Yusef Sohail <YusefSohail@gmail.com>
2022-04-13 18:33:41 -0700

    webgl / native parity

--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -3,6 +3,9 @@
 #include "gl/Renderer.h"
 #include "Window.h"
 #include "gltf.h"
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
 
 struct App {
     Window window;
@@ -18,7 +21,7 @@ void tick() {
     app->renderer.Render(app->camera, *app->scene);
     app->frameId++;
     
-    app->scene->children[0]->transform.rotation.y += .0001f;
+    app->scene->children[0]->transform.rotation.y += .001f;
 }
 
 void onCursorMoved(GLFWwindow* window, double x, double y) {
@@ -26,22 +29,33 @@ void onCursorMoved(GLFWwindow* window, double x, double y) {
 }
 
 int main(int argc, char* argv[]) {
+    std::cout << "CWD : " << std::filesystem::current_path() << std::endl;
+
+#ifndef EMSCRIPTEN
+    std::string path = "..\\..\\..\\..\\assets\\suzanne.glb";
+#else
+    std::string path = "./suzanne.glb";
+#endif
+
     app = std::make_unique<App>(App{
        Window::Create(600, 600, "Lawless"),
        gl::Renderer{},
        Matrix4::Perspective(110.0f, 1, 100),
-       gltf::Load("..\\..\\..\\..\\assets\\suzanne.glb"),
+       gltf::Load(path),
        0
     });
 
     std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "CWD : " << std::filesystem::current_path() << std::endl;
 
     glfwSetCursorPosCallback(app->window.window, onCursorMoved);
 
     app->scene->children[0]->transform.position = Vector3(0.0f, -0.0f, -30.0);
 
+#ifndef EMSCRIPTEN
     app->window.Tick([]() { tick(); });
+#else
+    emscripten_set_main_loop(tick, 0, true);
+#endif
 
     return 0;
 }
