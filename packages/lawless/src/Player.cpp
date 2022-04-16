64374910d0f646f4fc9a44cd491ce8334928bdd7
Yusef Sohail <YusefSohail@gmail.com>
2022-04-16 00:56:35 -0700

    basic platforming controls

--- packages/lawless/src/Player.cpp
+++ packages/lawless/src/Player.cpp
@@ -1,4 +1,3 @@
 #include "Player.h"
 
-const float Player::DEFAULT_SPEED = 2.0f;
 const ActorCreatorEntry Player::CREATORENTRY = std::make_pair("Player", [](const ActorInitializer& initializer) { return std::make_shared<Player>(initializer); });
--- packages/lawless/src/Player.h
+++ packages/lawless/src/Player.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Engine.h"
 #include "SceneAsset.h"
+#include <numbers>
 
 class Player : public Actor {
 public:
     using Super = Actor;
 
-    const static float DEFAULT_SPEED;
+    Vector3 velocity{};
+    bool canJump = true;
 
     Player(const ActorInitializer& initializer) : Actor(initializer) {
         this->root->AddChild(engine.GetAsset<SceneAsset>("suzanne.glb")->scene);
@@ -14,10 +16,32 @@ public:
 
     virtual void Tick(float deltaMs) override {
         Super::Tick(deltaMs);
-        this->GetTransform().rotation.y += .001f * deltaMs;
 
-        Vector3 movement(0, 0, engine.GetAxis("vertical"));
-        this->GetTransform().position += DEFAULT_SPEED * deltaMs *  movement.normalize();
+        Vector3 force(engine.GetAxis("horizontal") * .0001f, -0.0006f, 0.0f);
+
+        if (this->canJump && engine.GetKey(GLFW_KEY_SPACE)) {
+            force.y += 0.005f;
+            this->canJump = false;
+        }
+
+        this->velocity += deltaMs * force;
+
+        Transform& transform = this->GetTransform();
+        transform.position += deltaMs * velocity;
+
+        if (force.x > 0) {
+            transform.rotation.y = static_cast<float>(std::numbers::pi) / 2;
+        }
+        else if (force.x < 0) {
+            transform.rotation.y = -static_cast<float>(std::numbers::pi) / 2;
+        }
+
+        velocity *= 0.90f;
+        if (transform.position.y < 0) {
+            transform.position.y = 0;
+            this->velocity.y = 0;
+            this->canJump = true;
+        }
     }
 
     static const ActorCreatorEntry CREATORENTRY;
