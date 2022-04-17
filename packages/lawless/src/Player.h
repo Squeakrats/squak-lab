7e0d77de82366b1bbd8bd619ca561b2672d2c9a1
Yusef Sohail <YusefSohail@gmail.com>
2022-04-16 22:03:27 -0700

    tidy main

--- packages/lawless/src/Player.h
+++ packages/lawless/src/Player.h
@@ -7,8 +7,8 @@ class Player : public Actor {
 public:
     using Super = Actor;
 
+    float walkSpeed = .00004f;
     Vector3 velocity{};
-    bool canJump = true;
 
     Player(const ActorInitializer& initializer) : Actor(initializer) {
         this->root->AddChild(engine.GetAsset<SceneAsset>("suzanne.glb")->scene);
@@ -16,32 +16,12 @@ public:
 
     virtual void Tick(float deltaMs) override {
         Super::Tick(deltaMs);
-
-        Vector3 force(engine.GetAxis("horizontal") * .0001f, -0.0006f, 0.0f);
-
-        if (this->canJump && engine.GetKey(GLFW_KEY_SPACE)) {
-            force.y += 0.005f;
-            this->canJump = false;
-        }
-
-        this->velocity += deltaMs * force;
-
         Transform& transform = this->GetTransform();
+
+        Vector3 force(engine.GetAxis("horizontal"), engine.GetAxis("vertical"), 0.0f);
+        this->velocity += deltaMs * walkSpeed * force.normalize();
         transform.position += deltaMs * velocity;
-
-        if (force.x > 0) {
-            transform.rotation.y = static_cast<float>(std::numbers::pi) / 2;
-        }
-        else if (force.x < 0) {
-            transform.rotation.y = -static_cast<float>(std::numbers::pi) / 2;
-        }
-
-        velocity *= 0.90f;
-        if (transform.position.y < 0) {
-            transform.position.y = 0;
-            this->velocity.y = 0;
-            this->canJump = true;
-        }
+        this->velocity *= 0.9f;
     }
 
     static const ActorCreatorEntry CREATORENTRY;
--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -23,45 +23,20 @@ std::string assetDir = "./";
 std::string assetDir = "..\\..\\..\\..\\assets\\";
 #endif
 
-void createMap() {
-    auto asset = engine->GetAsset<SceneAsset>("cube.glb")->scene;
-    for (float x = -10.0f; x < 10.0f; x++) {
-        for (float y = -10.0f; y < 10.0f; y++) {
-            auto cube = std::make_shared<SceneNode>(Transform(
-                Vector3(x * 5.0f, y * 2.0f, -70.0)
-            ));
-
-            cube->AddChild(asset);
-            engine->GetScene()->AddChild(cube);
-        }
-    }
-}
-
 int main(int argc, char* argv[]) {
     engine = std::make_unique<Engine>(Engine::Create(1000, 1000, "Lawless", assetDir));
     engine->GetAssetManager().Register("glb", std::make_shared<gltf::GLBLoader>());
     engine->RegisterCreator(Player::CREATORENTRY);
     engine->SetRenderer(std::make_shared<gl::Renderer>());
 
-    engine->RegisterAxis("horizontal", { {
-        { GLFW_KEY_A, -1.0f},
-        { GLFW_KEY_D,  1.0f}
-    } });
+    engine->RegisterAxis("horizontal", { { { GLFW_KEY_A, -1.0f}, { GLFW_KEY_D,  1.0f } } });
+    engine->RegisterAxis("vertical", { { { GLFW_KEY_W, 1.0f}, { GLFW_KEY_S,  -1.0f } } });
 
     auto camera = std::make_shared<CameraNode>(Matrix4::Perspective(110.0f, 1, 100));
+    camera->transform.position.z = 70.0f;
     engine->SetCamera(camera);
 
-    createMap();
-
-    auto player = engine->Spawn<Player>(Transform(Vector3(0, 0.0, -70.0)));
-    engine->AddChild(player);
-
-    engine->OnTick([&camera, &player](float deltaMs) {
-        Vector3 difference = .1f * (player->GetTransform().position - camera->transform.position);
-        difference.z = 0.0f;
-
-        camera->transform.position += difference;
-    });
+    engine->AddChild(engine->Spawn<Player>());
 
     return run();
 }
