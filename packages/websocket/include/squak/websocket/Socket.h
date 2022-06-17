24c0c646a9664cc2d0dff6fb8cc957fdd33869db
Yusef Sohail <YusefSohail@gmail.com>
2022-06-18 00:03:34 +0900

    properly read back to back frames

--- packages/websocket/include/squak/websocket/Socket.h
+++ packages/websocket/include/squak/websocket/Socket.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <squak/net/tcp/Socket.h>
 #include <thread>
+#include <functional>
 
 namespace websocket {
 
+using OnMessage = std::function<void(std::string)>;
+
 class Socket {
   net::tcp::Socket socket;
+  OnMessage onMessage{};
+  std::thread thread{};
 
 public:
   Socket(net::tcp::Socket socket);
-  Socket(Socket&&) = default; 
+  Socket(Socket&&) = default;
+  ~Socket() { this->thread.join(); };
 
+  void OnMessage(OnMessage onMessage) { this->onMessage = onMessage; }
 };
 
 }
--- packages/websocket/src/Socket.cpp
+++ packages/websocket/src/Socket.cpp
@@ -1,8 +1,50 @@
-#include <squak/websocket/Socket.h>
+#include "utility.h"
 #include <array>
+#include <squak/websocket/Socket.h>
 
 namespace websocket {
 
-Socket::Socket(net::tcp::Socket socket) : socket(socket) {}
+Socket::Socket(net::tcp::Socket socket) : socket(socket) {
+  this->thread = std::thread{ [this]() {
+    while (true) {
+      uint8_t bytes2[2]{};
+      size_t read = this->socket.Read((char*)bytes2, 2);
+      if (read <= 0) {
+        break;
+      }
+
+      bool fin = (bytes2[0] & 0b10000000) != 0;
+      uint8_t opCode = bytes2[0] & 0b1111;
+      bool masked = (bytes2[1] & 0b10000000) != 0;
+      uint8_t length = bytes2[1] & 0b01111111;
+
+      Assert(fin == 1, "multi frame length");
+      Assert(opCode == 1, "multi frame length");
+      Assert(length <= 126, "multi frame length");
+      Assert(masked, "data from clients must be masked");
+
+      uint8_t mask[4]{};
+      read = this->socket.Read((char*)mask, 4);
+      if (read <= 0) {
+        break;
+      }
+
+      std::unique_ptr<uint8_t[]> payload = std::make_unique<uint8_t[]>(length);
+      read = this->socket.Read((char*)payload.get(), length);
+      if (read <= 0) {
+        break;
+      }
+
+      std::string message{};
+      message.resize(length);
+
+      for (size_t i = 0; i < length; i++) {
+        message[i] = payload[i] ^ mask[i % 4];
+      }
+
+      onMessage(message);
+    }
+  } };
+}
 
 };
