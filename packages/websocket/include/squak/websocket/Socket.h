f861938395c69805b5df65f07b9a96e3962ad44e
Yusef Sohail <YusefSohail@gmail.com>
2022-06-18 12:36:59 +0900

    clean up websocket data polling

--- packages/websocket/include/squak/websocket/Socket.h
+++ packages/websocket/include/squak/websocket/Socket.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <squak/net/tcp/Socket.h>
-#include <thread>
 #include <functional>
+#include <array>
 
 namespace websocket {
 
@@ -10,7 +10,8 @@ using OnMessage = std::function<void(std::string)>;
 class Socket {
   std::unique_ptr<net::tcp::Socket> socket;
   OnMessage onMessage{};
-  std::thread thread{};
+  std::array<uint8_t, 4096> buffer{};
+  size_t buffered = 0;
 
   void Poll();
 
@@ -18,7 +19,6 @@ public:
   Socket(std::unique_ptr<net::tcp::Socket> socket);
   Socket(const Socket&) = delete;
   Socket(Socket&&) = delete;
-  ~Socket() { this->thread.join(); };
 
   void OnMessage(OnMessage onMessage) { this->onMessage = onMessage; }
 };
--- packages/websocket/src/Socket.cpp
+++ packages/websocket/src/Socket.cpp
@@ -5,32 +5,42 @@
 namespace websocket {
 
 void Socket::Poll() {
-  uint8_t bytes2[2]{};
-  size_t read = this->socket->Read((char*)bytes2, 2);
-  if (read <= 0) {
-    Panic("not enough data");
+  size_t read = this->socket->Read((char*)this->buffer.data() + this->buffered,
+                                   this->buffer.size() - this->buffered);
+
+  if (read == 0) {
+    // todo - emit socket closed event
+    return;
   }
 
-  bool fin = (bytes2[0] & 0b10000000) != 0;
-  uint8_t opCode = bytes2[0] & 0b1111;
-  bool masked = (bytes2[1] & 0b10000000) != 0;
-  uint8_t length = bytes2[1] & 0b01111111;
+  this->buffered += read;
+
+  if (this->buffered < 2) {
+    // missing fin, opCode & length
+    return;
+  }
+
+  bool fin = (this->buffer[0] & 0b10000000) != 0;
+  uint8_t opCode = this->buffer[0] & 0b1111;
+  bool masked = (this->buffer[1] & 0b10000000) != 0;
+  uint8_t length = this->buffer[1] & 0b01111111;
 
   Assert(fin == 1, "multi frame length");
   Assert(length <= 126, "multi frame length");
   Assert(masked, "data from clients must be masked");
 
-  uint8_t mask[4]{};
-  read = this->socket->Read((char*)mask, 4);
-  if (read <= 0) {
-    Panic("not enough data");
+  if (this->buffered < 6) {
+    // missing mask
+    return;
+  }
+  uint8_t* mask = this->buffer.data() + 2;
+
+  if (this->buffered < 6 + length) {
+    // missing payload
+    return;
   }
 
-  std::unique_ptr<uint8_t[]> payload = std::make_unique<uint8_t[]>(length);
-  read = this->socket->Read((char*)payload.get(), length);
-  if (read <= 0) {
-    Panic("not enough data");
-  }
+  uint8_t* payload = this->buffer.data() + 6;
 
   if (opCode == 1) {
     std::string message{};
@@ -46,6 +56,8 @@ void Socket::Poll() {
   } else {
     Panic("unhandled op");
   }
+
+  this->buffered = 0;
 }
 
 Socket::Socket(std::unique_ptr<net::tcp::Socket> socket)
