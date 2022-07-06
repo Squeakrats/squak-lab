211ffa2c5db79f0129872933e8715a6c136665b8
Yusef Sohail <YusefSohail@gmail.com>
2022-07-06 16:15:56 -0500

    simplify lawless setup

--- packages/lawless/src/main.cpp
+++ packages/lawless/src/main.cpp
@@ -8,28 +8,6 @@
 #include <squak/gltf.h>
 #include <squak/websocket/Server.h>
 
-void server(std::string path, std::string assetDir) {
-  Engine& engine = Engine::engine;
-
-#ifndef EMSCRIPTEN
-  websocket::Server server{};
-  server.OnConnection([&engine](websocket::Socket& socket) {
-    socket.OnMessage(
-      [&engine](std::string m) { engine.Invoke(RPC::Parse(json::Parse(m))); });
-  });
-
-  server.Listen("0.0.0.0", 1338);
-#endif
-
-  spawn(path, assetDir + " client");
-  engine.Run();
-}
-
-void client() {
-  Engine& engine = Engine::engine;
-  engine.Run();
-}
-
 int main(int argc, char* argv[]) {
   std::string path = argv[0];
   std::string assetDir = argv[1];
@@ -40,14 +18,23 @@ int main(int argc, char* argv[]) {
     gl::CreateRenderer(engine.GetSize().first, engine.GetSize().second));
   engine.GetScene()->AddChild(
     engine.GetAssetManager().Get<SceneAsset>("map.glb")->scene);
+  engine.Spawn<PlayerCamera>();
 
-  auto camera = engine.Spawn<PlayerCamera>();
-
+  std::unique_ptr<websocket::Server> rpcServer{};
   if (argc == 2) {
-    server(path, assetDir);
-  } else {
-    client();
+    rpcServer = std::make_unique<websocket::Server>();
+    rpcServer->OnConnection([](websocket::Socket& socket) {
+      socket.OnMessage([](std::string m) {
+        Engine::engine.Invoke(RPC::Parse(json::Parse(m)));
+      });
+    });
+
+    rpcServer->Listen("0.0.0.0", 1338);
+
+    spawn(path, assetDir + " client");
   }
+
+  engine.Run();
 
   return 0;
 }
